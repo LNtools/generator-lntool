a7e7001793773d74a7a74af28a5d9130f110104d
cbertelegni <cristianbertelegni@gmail.com>
2017-03-07 15:33:31 -0300

    fix varios

--- generators/app/templates/source/js/main.js
+++ generators/app/templates/source/js/main.js
@@ -8,4 +8,22 @@ $(function () {
         .loader.hide();
 
 
+
+
+    /**
+    *
+    * Update data from autotune live preview
+    *
+    */
+    // OL.pymChild.onMessage('updateData', function(data) {
+    //     data = JSON.parse(data);
+    //     updateDataSet(data);
+
+    // });
+
+    // function updateDataSet(_data){
+    //     /** update autotune app */
+    //     console.info(_data);
+    // }
+
 });
