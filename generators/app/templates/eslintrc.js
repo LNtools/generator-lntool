f6ef78a95a71a56b310d218b49be52d2b714c8fd
Cristian Bertelegni <cristianbertelegni@gmail.com>
2019-01-04 16:27:29 -0300

    agrego reglas al eslint  y editorconfig

--- generators/app/templates/eslintrc.js
+++ generators/app/templates/eslintrc.js
@@ -10,6 +10,30 @@ module.exports = {
     "rules": {
         'quotes': ["warn", 'single'],
         "semi": 2,
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        "indent": ["warn", 2,
+          {
+            "SwitchCase": 1,
+            "MemberExpression": 1,
+            "ObjectExpression": 1
+          }],
+        "no-loop-func": "error",
+        "no-dupe-keys": "error",
+        "no-empty": "error",
+        "block-scoped-var": "error",
+        "dot-notation": "error",
+        "no-redeclare": "error",
+        "no-self-assign": "error",
+        "no-useless-escape": "error",
+        "comma-spacing": ["warn", { "before": false, "after": true }],
+        "key-spacing": ["error",
+          {
+            "beforeColon": false,
+            "afterColon": true
+          }],
+        "keyword-spacing": ["error", { "before": true }],
+        "no-debugger": process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        "no-console": [process.env.NODE_ENV === 'production' ? 'error' : 'off',
+            { allow: ["warn", "error"]
+        }],
     }
 }
